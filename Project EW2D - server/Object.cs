7ca67d304ee7ddfc92955562f19375e402c1aef5
LaurentQUERSIN <quersin.laurent@gmail.com>
2015-10-09 17:20:11 +0200

    fix start up trouble

--- Project EW2D - server/Object.cs
+++ Project EW2D - server/Object.cs
@@ -60,7 +60,6 @@ namespace Project_EW2D___server
             name = pName;
             weapon = Weapons.instance.getWeapon(WeaponTypes.RAPID_FIRE);
             lastUpdate = updateTime;
-
         }
         public Player(myGameObject obj, long updateTime)
         {
